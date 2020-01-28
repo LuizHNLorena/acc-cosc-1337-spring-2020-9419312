42b743b586594ab8b2c72501c50e20b093d37534
9419312 <9419312@HLC2412-14>
2020-01-27 18:16:35 -0600

    hello world function

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,14 @@
 //Write the include statement for types.h here
 
 //Write include for capturing input from keyboard and displaying output to screen
+#include "output.h"
+
 
 
 
 int main() 
 {
+	say_hello();
 
 	return 0;
 }
