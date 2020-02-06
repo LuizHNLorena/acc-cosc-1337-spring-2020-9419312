b9c75e068d60fdbe688a2e9c6d044ea104885e91
9419312 <9419312@HLC2412-14>
2020-02-05 19:10:49 -0600

    assignment 2 - not done

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,13 @@
 //Write the function code that returns the product of hours and hourly_rate.
+#include<iostream>
+#include "decision.h"
+
+int gross_pay(int hours, int hourly_rate)
+{
+	int hours = 40;
+	if (gross_pay > = 40)
+		cout << "regular hours";
+
+		break; 
+	return 0;
+}
