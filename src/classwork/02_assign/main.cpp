d06594fcb5f4a661d3daa0632d9c8e08f403dff0
marily_mini <marily_mini@outlook.com>
2020-02-08 14:15:48 -0600

    finalcommit for assignment 2

--- src/classwork/02_assign/main.cpp
+++ src/classwork/02_assign/main.cpp
@@ -1,5 +1,5 @@
 //Write the include statement for decisions.h here
-#include"decisions.h"
+#include"decision.h"
 
 //Write include for capturing input from keyboard and displaying output to screen
 #include<iostream>
@@ -7,10 +7,10 @@
 
 
 //Write namespace using statements for cout and cin
-using std::cout; using std::cin;
+using namespace std;
 
 
-int main() 
+int main()
 {
 	//create a double variable named hours
 	double hours;
@@ -21,21 +21,21 @@ int main()
 	//save the input to variables hours and hourly_rate
 	cout << "input for hours and hourly_rate: ";
 	cin >> hours >> hourly_rate;
-	
-	
+
+
 
 
 	//Create a double variable named gross_salary
 	double gross_salary = 0.0;
 
 	//Call the gross_pay function and save its return value to the gross_salary variable
-	finalypay = gross_salary(hours, hourly_rate);
+	gross_salary = gross_pay(hours, hourly_rate);
 
 	//Display the gross_salary variable as follows:
-	
+
 
 	//Example ---->>>     Gross pay: 150.00  
-	cout << "Gross pay: " << gross_salary << indl;
+	cout << "Gross pay: " << gross_salary << endl;
 
 
 	return 0;
