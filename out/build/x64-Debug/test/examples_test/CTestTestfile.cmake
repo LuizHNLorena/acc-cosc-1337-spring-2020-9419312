7f6dcc95dc5bd04248f401fe076f87688cafa29d
marily_mini <marily_mini@outlook.com>
2020-02-11 14:15:03 -0600

    clone repo 2/11/2020

---
+++ out/build/x64-Debug/test/examples_test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mvelazquez01/Desktop/repos/test/examples_test
+# Build directory: C:/Users/mvelazquez01/Desktop/repos/out/build/x64-Debug/test/examples_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("02_module_test")
+subdirs("03_module_test")
