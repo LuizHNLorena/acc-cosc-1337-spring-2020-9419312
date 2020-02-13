026ff2472b7a38655b847d7bd00562aa708fdd62
9419312 <9419312@HLC2412-14>
2020-02-12 19:02:04 -0600

    homework 3

--- src/classwork/03_assign/loops.cpp
+++ src/classwork/03_assign/loops.cpp
@@ -10,3 +10,16 @@ returns 120
 
 DON'T FORGET TO WRITE TEST CASE.  See file loop_test.cpp
 */
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int factorial(int num) 
+{
+	int result = 1;
+	for (int i = 1; i <= num; ++i) {
+		result *= i;
+	}
+	return result;
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,15 +1,25 @@
-#write includes statements
+//#write includes statements
 
 //write using statements for cin and cout
-
+#include<iostream>
 
 /*
-Use a do while loop to prompt the user for 
+Use a do while loop to prompt the user for
 a number, call the factorial function, and display the number's
 factorial.  Also, loop continues as long as user wants to.
 */
-int main() 
-{
+using std::cout;
+using std::cin;
 
+int factorial(int num);
+
+int main() {
+	int n;
+	do {
+		cout << "Enter a number(negative to stop): ";
+		cin >> n;
+		if (n >= 0) {
+			cout << "Factorial of " << n << " is " << factorial(n) << "\n";
+		}
+	} while (n >= 0);
 	return 0;
-}
