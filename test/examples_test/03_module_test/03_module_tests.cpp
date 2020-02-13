c1a1ed70630797623aba191375974d6b59676508
9419312 <9419312@HLC2412-14>
2020-02-12 19:01:53 -0600

    test case 3

--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "while.h"
+#include "value_ref.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -11,3 +12,15 @@ TEST_CASE("Test sum of squares function")
 	REQUIRE(sum_of_square(4) == 30);
 
 }
+TEST_CASE("Test value and ref function") {
+	int num1 = 5, num2 = 10;
+	
+	pass_by_value_and_ref(num1, num2);
+
+	REQUIRE(num1 == 5);
+	REQUIRE(num2 == 50);
+
+}
+
+
+
