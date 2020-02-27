2fc4f11b1511bfe73d03ef5d214223549a7b3faa
marily_mini <marily_mini@outlook.com>
2020-02-27 17:59:26 -0600

    homework 4

--- src/homework/04_vectors/main.cpp
+++ src/homework/04_vectors/main.cpp
@@ -1,3 +1,8 @@
+#include<string>
+#include"vectors.h"
+#include<iostream>
+
+using std::cout; using std::cin; using std::string;
 /*
 use a vector of int with values 8, 4, 20, 88, 66, 99
 
@@ -7,8 +12,40 @@ and display them to screen.
 Program continues until user decides to exit.
 
 */
-int main() 
+int main()
 {
+	int choice = 1;
+	while (choice == 1 || choice == 2) {
+		cout << " Prss 1 for Get Max from vector or press 2 for primes\n";
+		cin >> choice;
+		if (choice == 1) {
+			vector<int> vec;
+			int i = 1;
+			while (i <= 5) {
+				int num;
+				cout << "Num" << i << ":\n";
+				vec.push_back(num);
+				++i;
+			}
+			int max = get_get_max_from_vector(vec);
+			cout << "Max number from vectors is : " << max << "\n";
 
+
+		}
+		else {
+			int num;
+			cout << "Enter a number \n";
+
+			cin >> num;
+			vector<int>primes = vector_of_primes(num);
+			cout << " list of primes: \n \n";
+
+			for (int prime : primes) {
+				cout << prime << "\n";
+			}
+			cout << "\n";
+		}
+
+	}
 	return 0;
 }
--- test/homework_test/04_vectors_test/04_vectors_tests.cpp
+++ test/homework_test/04_vectors_test/04_vectors_tests.cpp
@@ -1,7 +1,50 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "vectors.h"
+#include<vector>
+using std::vector;
+
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+
+}
+// prime numbers tested 2, 4, 43, 44
+
+TEST_CASE(" Verify if is_prime" ) {
+	REQUIRE(is_prime(2) == true);
+	REQUIRE(is_prime(4) == true);
+	REQUIRE(is_prime(43) == true);
+	REQUIRE(is_prime(44) == true);
 }
 
+
+//vector w values 3,8,1,99,1000
+//vector w values 15, 12, 11, 99, 88
+//vector w values 150, 120, 11, 990, 88888
+
+
+TEST_CASE(" Verify if get_max_from_vector") {
+	vector<int> vec1 = { 3, 8, 1, 99, 1000 };
+	REQUIRE(get_get_max_from_vector(vec1) == 1000);
+
+	vector<int> vec2 = { 15, 12, 11, 99, 88 };
+    REQUIRE(get_get_max_from_vector(vec2) == 99);
+
+	vector<int> vec3 = { 150, 120, 11, 990, 88888 };
+	REQUIRE(get_get_max_from_vector(vec3) == 88888);
+	
+}
+
+//10 vector with values 2, 3, 5, 7
+//50 vector with values 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47
+
+TEST_CASE("Verify vector_of_primes") {
+	vector<int> assert1 = { 2, 3, 5, 7 };
+	REQUIRE(vector_of_primes(10) == assert1);
+
+	vector<int> assert2 = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+	REQUIRE(vector_of_primes(50) == assert2);
+
+
+}
