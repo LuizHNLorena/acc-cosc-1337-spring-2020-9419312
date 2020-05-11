dfd85c9a8b0e69faee56a860cc8a0f0b1ce6ed4f
marily_mini <marily_mini@outlook.com>
2020-05-10 22:50:16 -0500

    HW11

--- src/homework/tic_tac_toe/tic_tac_toe_data.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_data.cpp
@@ -4,3 +4,57 @@
 #include"tic_tac_toe_4.h"
 #include<string>
 
+void TicTacToeData::save_pegs(const std::vector<std::unique_ptr<TicTacToe>>& games)
+{
+	std::ofstream file_out(file_name, std::ios_base::trunc);
+
+	for (auto& game : games)
+	{
+		for (auto peg : game->get_pegs())
+		{
+			file_out << peg;
+		}
+		file_out << game->get_winner();
+		file_out << "\n";
+	}
+
+	file_out.close();
+}
+
+std::vector<std::unique_ptr<TicTacToe>> TicTacToeData::get_games()
+{
+
+	std::vector<std::unique_ptr<TicTacToe>> boards;
+	std::ifstream read_file(file_name);
+
+	while (read_file.is_open())
+	{
+
+		std::vector<std::string> pegs;
+		std::string line;
+
+		while (getline(read_file, line))
+		{
+			for (int ch = 0; ch < line.size() - 1; ch++)
+			{
+				std::string chstring(1, line[ch]);
+				pegs.push_back(chstring);
+			}
+			std::string winner = std::string(1, line.back());
+			std::unique_ptr<TicTacToe> board;
+
+			if (pegs.size() == 9)
+			{
+				board = std::make_unique<TicTacToe3>(pegs, winner);
+			}
+			else if (pegs.size() == 16)
+			{
+				board = std::make_unique<TicTacToe4>(pegs, winner);
+			}
+
+			boards.push_back(std::move(board));
+		}
+		read_file.close();
+	}
+	return boards;
+}
--- src/homework/tic_tac_toe/tic_tac_toe_data.h
+++ src/homework/tic_tac_toe/tic_tac_toe_data.h
@@ -1,26 +1,26 @@
 //h
-// a) Add class constructor in .h file
-
-
-//TicTacToe(std::vector<string> p, string win) //more code here to
-//initialize the pegs vector with pand initialize winner.
-
-//b) Add const class function get_pegs that returns a vector of strings
-//return the pegs vector
-
 #include<vector>
 #include"tic_tac_toe.h"
 #include<fstream>
+#include<memory>
 #include<string>
 
-class TicTacToeManager
+#ifndef  TIC_TAC_TOE_DATA_H
+#define TIC_TAC_TOE_DATA_H
+
+
+
+class TicTacToeData	
 {
 public:
+	void save_pegs(const std::vector<std::unique_ptr<TicTacToe>>& games);
+	std::vector<std::unique_ptr<TicTacToe>> get_games();
 
-protected:
 
 
 private:
-
+	const std::string file_name{ "tictactoe.dat" };
 };
+
+#endif 
 
